e6247021a4b24d87afed48f9842da9572472a432
gdlyeabkov <gdlyeabkov@gmail.com>
2022-03-30 17:44:41 +0300

    add_toggle_focus

---
+++ Assets/Editor/SpriteAnimationDrager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Linq.Expressions;
+
+[CustomEditor(typeof(Transform))]
+public class SpriteAnimationDrager : Editor
+{
+
+    public void OnSceneGUI()
+    {
+        Event currentEvent = Event.current;
+        EventType currentEventType = currentEvent.type;
+        EventType dragUpdatedEventType = EventType.DragUpdated;
+        bool isDragUpdatedEventType = currentEventType == dragUpdatedEventType;
+        if (isDragUpdatedEventType)
+        {
+            EditorApplication.ExecuteMenuItem("Window/Animation/Animation");
+        }
+    }
+
+}
